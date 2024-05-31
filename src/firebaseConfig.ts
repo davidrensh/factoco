14678579ac89c695425a28e2c941db2d5358cf38
Shanhua Ren <Davidrensh@hotmail.com>
2024-05-31 19:34:41 -0400

    secret

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,13 +3,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 const firebaseConfig = {
-    apiKey: "AIzaSyDvsrBY3PvBBDcDGZLVAYLA6pALQkmv6vU",
-    authDomain: "factocor.firebaseapp.com",
-    projectId: "factocor",
-    storageBucket: "factocor.appspot.com",
-    messagingSenderId: "693366799762",
-    appId: "1:693366799762:web:9909a3533e8c94f814954b",
-    measurementId: "G-Z4BL36HWMQ"
+ 
   };
   
 
